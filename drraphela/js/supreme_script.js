91e07e455ae975e9cb221a2507e900d4fcd5215a
Emmanuel Moila <emmoila@users.noreply.github.com>
2019-11-10 11:32:14 +0200

    Add files via upload

---
+++ drraphela/js/supreme_script.js
@@ -0,0 +1,87 @@
+var top_menu_height = 0;
+jQuery(function($) {
+		$(window).load( function() {
+			$('.external-link').unbind('click');	
+		});
+		
+        $(document).ready( function() {
+
+            // load google map
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCShPR36rLY0UlHbf6qofFnC2JdzJJpvzw&sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+
+        top_menu_height = $('.supreme-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({ target: '#supreme-nav-bar', offset: top_menu_height + 10 });
+		$('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e){
+            e.preventDefault();
+            scrollTo('#supreme-top');
+        });
+
+        // scroll to specific id when click on menu
+        $('.supreme-top-menu .navbar-nav a').click(function(e){
+            e.preventDefault(); 
+            var linkId = $(this).attr('href');
+            scrollTo(linkId);
+            if($('.navbar-toggle').is(":visible") == true){
+                $('.navbar-collapse').collapse('toggle');
+            }
+            $(this).blur();
+            return false;
+        });
+		/*
+		var dt = window.atob('IHwgRGVzaWduOiA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tL3RtLTM5NS11cmJhbmljIiB0YXJnZXQ9Il9wYXJlbnQiPlVyYmFuaWM8L2E+'); // decode the string
+		var div = document.getElementById('footer-line');
+		div.innerHTML = div.innerHTML + dt;
+		*/
+        // to stick navbar on top
+        $('.supreme-top-menu ').stickUp();
+
+        // gallery category
+        $('.supreme-gallery-category a').click(function(e){
+            e.preventDefault(); 
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function(){
+                if($(this).is(":visible") == true){
+                   $(this).hide();
+                };
+            });
+            $(linkClass).fadeIn();  
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+                                    rel: function(){
+                                        return $(this).data('group');
+
+                                    }
+        });
+    });
+});
+
+function initialize() {
+    var mapOptions = {
+      zoom: 17,
+      center: new google.maps.LatLng(-25.66638,27.2352113,17)
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+}
+
+// scroll animation 
+function scrollTo(selectors)
+{
+
+    if(!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+
+}
